78ded850e7a4e985ff385d77756c57653d7e71cc
Nick Earl <nick.r.earl@gmail.com>
2022-06-19 01:01:07 -0400

    added in and integrated front end with server

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.use(express.static('public'))
+
 function filterByQuery(query, animalsArray){
     let peronalityTraitsArray = []
     let filteredResults = animalsArray
@@ -98,7 +100,23 @@ app.post('/api/animals', (req, res)=>{
         const animal = createNewAnimal(req.body, animals)
     }
 
-    res.json(animal)
+    res.json(animals)
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'))
+})
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'))
+})
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
 app.listen(PORT, ()=>{
